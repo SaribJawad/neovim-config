2489903ee1505d53e4406cd7cb59648a93e4ae1d
SaribJawad <msssarib12@gmail.com>
2025-05-04 21:35:01 +0500

    first commit

---
+++ init.lua
@@ -0,0 +1,33 @@
+require("core.option")
+require("core.keymaps")
+
+-- LAZY NVIM Package manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		error("Error cloning lazy.nvim:\n" .. out)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+
+require("lazy").setup({
+	require("plugins.neotree"),
+	require("plugins.colortheme"),
+	require("plugins.bufferline"),
+	require("plugins.lualine"),
+	require("plugins.treesitter"),
+	require("plugins.telescope"),
+	require("plugins.lsp"),
+	require("plugins.autocompletion"),
+	require("plugins.autoformating"),
+	require("plugins.gitsigns"),
+	require("plugins.alpha"),
+	require("plugins.indent-blankline"),
+	require("plugins.misc"),
+})
